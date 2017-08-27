4fc23c70daecdeb95ebf103b6275dbd9944c2894
Sarah Pierce <spierce@austin.rr.com>
2017-08-27 16:15:40 -0500

    Added lists for icing, sprinkles, and filling

--- DonutCombos.py
+++ DonutCombos.py
@@ -1,1 +1,28 @@
 """ DonutCombos - Utility app to create code for multi-donuts-mod """
+
+"""
+lists
+"""
+icings = [ 'none', 'strawberry', 'chocolate', 'blueberry', 'vanilla' ]
+sprinkles = [ 'none', 'green', 'blue', 'red', 'yellow', 'orange', 'rainbow' ]
+fillings = [ 'none', 'strawberry', 'chocolate', 'vanilla', 'cookieDough', 'brownieBatter' ]
+
+"""
+create item1 list
+"""
+
+"""
+create item2 list
+"""
+
+"""
+create lang list
+"""
+
+"""
+create recipe list
+"""
+
+"""
+output lists for copy/paste
+"""
