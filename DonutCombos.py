8298cebc5aec6e6b0e147a92caa6603839fd36d0
Sarah Pierce <spierce@austin.rr.com>
2017-08-27 16:59:21 -0500

    Added code to create & output list for ModItems

--- DonutCombos.py
+++ DonutCombos.py
@@ -6,10 +6,19 @@ lists
 icings = [ 'none', 'strawberry', 'chocolate', 'blueberry', 'vanilla' ]
 sprinkles = [ 'none', 'green', 'blue', 'red', 'yellow', 'orange', 'rainbow' ]
 fillings = [ 'none', 'strawberry', 'chocolate', 'vanilla', 'cookieDough', 'brownieBatter' ]
+item1 = []
+item2 = []
+lang = []
+recipe = []
 
 """
 create item1 list
 """
+for icing in icings:
+    for sprinkle in sprinkles:
+        for filling in fillings:
+            item1.append( 'public static ItemFood ' + icing + sprinkle.capitalize() + filling.capitalize() + 'Donut;' )
+
 
 """
 create item2 list
@@ -26,3 +35,6 @@ create recipe list
 """
 output lists for copy/paste
 """
+print ('ModItems: \n')
+for item in item1:
+    print (item)
